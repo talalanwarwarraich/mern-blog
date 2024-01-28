50e5c43f7bf407f786a160a420c267d0a6f22aff
talalanwarwarraich <tala.anwar.warraich@gmail.com>
2024-01-28 16:08:16 +0500

    create separate logo component.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,20 +2,13 @@ import { Button, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
+import Logo from './ui/Logo';
 
 const Header = () => {
   const path = useLocation().pathname;
   return (
     <Navbar className='border-b-2'>
-      <Link
-        to='/'
-        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
-      >
-        <span className='px-2 py-1 mr-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-          MERN
-        </span>
-        blog
-      </Link>
+      <Logo />
       <form>
         <TextInput
           type='text'
