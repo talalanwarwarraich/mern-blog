ab27e107ef9924ede4b0f302b82917c84ad385a7
talalanwarwarraich <tala.anwar.warraich@gmail.com>
2024-01-31 14:22:49 +0500

    complete image upload functionality.

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,18 +1,119 @@
-import { Button, TextInput } from 'flowbite-react';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { Alert, Button, TextInput } from 'flowbite-react';
+import { useEffect, useRef, useState } from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import { useSelector } from 'react-redux';
+
 const DashProfile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const [image, setImage] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+  const [uploadProgress, setUploadProgress] = useState(null);
+  const [fileUploadError, setFileUploadError] = useState(null);
+  const filePickerRef = useRef();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    setImage(file);
+    setImageUrl(URL.createObjectURL(file));
+  };
+
+  useEffect(() => {
+    if (!image) return;
+
+    uploadImage();
+  }, [image]);
+
+  const uploadImage = async () => {
+    setFileUploadError(null);
+    const storage = getStorage();
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadProgress(progress.toFixed(0));
+        console.log(progress, snapshot);
+      },
+      (error) => {
+        console.error(error);
+        setFileUploadError(
+          'Could not upload iamge(File must be less than 2MB)'
+        );
+        setUploadProgress(null);
+        setImageUrl(null);
+        setImage(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageUrl(downloadURL);
+        });
+      }
+    );
+  };
+
+  const showUploadProgress = () => {
+    if (!uploadProgress) return;
+
+    if (uploadProgress < 0 || uploadProgress >= 100) return;
+
+    return (
+      <CircularProgressbar
+        value={uploadProgress}
+        text={`${uploadProgress}%`}
+        strokeWidth={5}
+        styles={{
+          root: {
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+          },
+          path: {
+            stroke: `rgba(62,152,199, ${uploadProgress / 100})`,
+          },
+        }}
+      />
+    );
+  };
+
   return (
     <div className='max-w-lg max-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>profile</h1>
       <form className='flex flex-col gap-4'>
-        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
+        <input
+          onChange={handleImageChange}
+          type='file'
+          accept='image/*'
+          ref={filePickerRef}
+          className='hidden'
+        />
+        <div
+          className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'
+          onClick={() => filePickerRef.current.click()}
+        >
+          {showUploadProgress()}
           <img
-            src={currentUser?.profilePic}
+            src={imageUrl || currentUser?.profilePic}
             alt='user'
-            className='rounded-full w-full object-cover border-8 border-[lightgray]'
+            className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${
+              uploadProgress && uploadProgress < 100 ? 'opacity-60' : ''
+            }`}
           />
         </div>
+        {fileUploadError && <Alert color='failure'>{fileUploadError}</Alert>}
         <TextInput
           type='text'
           id='username'
