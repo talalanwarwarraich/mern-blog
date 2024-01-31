aaa65175e3957a4b6da076f006b575f22f2d25de
talalanwarwarraich <tala.anwar.warraich@gmail.com>
2024-01-31 12:46:57 +0500

    complete dashboard sidebar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashProfile from '../components/DashProfile';
+import DashSidebar from '../components/DashSidebar';
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) setTab(tabFromUrl);
+  }, [location.search]);
 
-export default Dashboard
+  const renderTab = () => {
+    if (tab === 'profile') {
+      return <DashProfile />;
+    }
+  };
+
+  return (
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      {/* sidebar */}
+      <div className='md:w-56'>
+        <DashSidebar />
+      </div>
+      {/* profile */}
+      {renderTab()}
+    </div>
+  );
+};
+
+export default Dashboard;
